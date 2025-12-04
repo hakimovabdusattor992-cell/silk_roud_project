28f231a5c8b43c9955ad766b11a7465b99c8e5e9
Abdusattor <hakimovabdusattor992@gmail.com>
2025-12-04 13:37:02 +0500

    front-end tugadi opshi pashti footerdan tashqari

--- public/js/mainCard.js
+++ public/js/mainCard.js
@@ -113,3 +113,31 @@ document.getElementById("buyNowBtn").addEventListener("click", function () {
 document.addEventListener("DOMContentLoaded", function () {
   initThumbnails();
 });
+
+
+let index = 0;
+
+function slider() {
+  const slider = document.getElementById("brand-slider");
+  const slides = document.querySelectorAll(".slide");
+  const totalSlides = slides.length;
+
+  // Ekranda nechta rasm ko‘rinishini aniqlash
+  let visibleSlides = 4; // Desktop default
+  if (window.innerWidth < 768) visibleSlides = 2; // Mobile
+  else if (window.innerWidth < 1024) visibleSlides = 3; // Tablet
+
+  index++;
+
+  if (index > totalSlides - visibleSlides) {
+    index = 0;
+  }
+
+  // Har bir rasm qancha foiz joy olishini hisoblash
+  const movePercent = 100 / visibleSlides;
+
+  slider.style.transform = `translateX(-${index * movePercent}%)`;
+}
+
+// Har 3 soniyada siljish
+setInterval(slider, 3000);
