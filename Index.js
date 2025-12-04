aaa8cfe3c76275de9d6490fec3efe4cc08068247
Abdusattor <hakimovabdusattor992@gmail.com>
2025-12-04 17:25:19 +0500

    oxirgi yurish

--- Index.js
+++ Index.js
@@ -21,17 +21,18 @@ const db = new Pool({
 
 
 app.get("/", async (req, res) => {
-  const generals = await db.query("SELECT * FROM generals_cards ORDER BY id ASC");
-
-  res.render("index", {
-    generals_cards: generals.rows
+    // const result = await db.query("SELECT * FROM generals_cards LIMIT 6");
+    
+    res.render("/index");
   });
-});
 
 app.get("/singIn", async (req, res) => {
- 
+    const { name, email } = req.body;
+  await db.query("INSERT INTO users (name, email) VALUES ($1, $2)", [name, email]);
  res.render("singIn");
 });
+
+
 app.get("/singUp", async (req, res) => {
  
  res.render("singUp");
@@ -46,18 +47,5 @@ app.get("/mainCard", async (req, res) => {
 });
 
 
-// Create user
-app.post("/add", async (req, res) => {
- const { name, email } = req.body;
- await db.query("INSERT INTO users (name, email) VALUES ($1, $2)", [name, email]);
- res.redirect("/");
-});
 
-// Delete user
-app.post("/delete/:id", async (req, res) => {
- await db.query("DELETE FROM users WHERE id = $1", [req.params.id]);
- res.redirect("/");
-});
-
-// Start server
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
